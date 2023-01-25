aaab5451eaee28cfb8ca740e33d5a6a6ed02a8cd
LucasAlves17 <lukasmartinsalves17@gmail.com>
2023-01-25 16:54:48 -0300

    finish api functions

--- src/api/api.js
+++ src/api/api.js
@@ -22,45 +22,59 @@ async function tokenGenerator(email) {
   return { status: response.status, body: await response.json() };
 }
 
-async function getAllInvoices(filters) {
-  const response = await fetch(`${API_BASE_URL}/users/login`, {
-    method: "POST",
+async function getAllInvoices(filters = {}) {
+  const params = createQueryParams(filters);
+
+  const response = await fetch(`${API_BASE_URL}/invoices?${params}`, {
+    method: "GET",
     mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ token: email }),
+    headers: { Authorization: localStorage.getItem("Authorization") },
   });
 
   return { status: response.status, body: await response.json() };
 }
+
 async function getInvoice(id) {
-  const response = await fetch(`${API_BASE_URL}/users/login`, {
-    method: "POST",
+  const response = await fetch(`${API_BASE_URL}/invoices/${id}`, {
+    method: "GET",
     mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ token: email }),
+    headers: { Authorization: localStorage.getItem("Authorization") },
   });
 
   return { status: response.status, body: await response.json() };
 }
-async function editInvoiceEmails(emails) {
-  const response = await fetch(`${API_BASE_URL}/users/login`, {
-    method: "POST",
+
+async function editInvoiceEmails(id, emails) {
+  const response = await fetch(`${API_BASE_URL}/invoices/${id}`, {
+    method: "PUT",
     mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ token: email }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
+    },
+    body: JSON.stringify({ emails: emails }),
   });
 
   return { status: response.status, body: await response.json() };
 }
 async function createInvoice(invoice) {
-  const response = await fetch(`${API_BASE_URL}/users/login`, {
+  const response = await fetch(`${API_BASE_URL}/invoices`, {
     method: "POST",
     mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ token: email }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
+    },
+    body: JSON.stringify(invoice),
   });
 
   return { status: response.status, body: await response.json() };
+}
+
+function createQueryParams(params) {
+  return Object.keys(params)
+    .map((key) => `${key}=${encodeURI(params[key])}`)
+    .join("&");
 }
 
 export {
